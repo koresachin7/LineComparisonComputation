e924cc07f3d071329a89194ecb45c0bdaee214cc
koresachin7 <koresachin7@gmail.com>
2021-09-07 07:03:59 +0530

    Added : Welcome sms

---
+++ src/com/bridgelabz/LineComparison.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Line Comparison Computation Program ");
+    }
+
+}
